5bc99101384fb99929de8f297dbe404548d4a655
RvBurik <ricardovanburik@hotmail.com>
2017-06-09 15:09:34 +0200

    Updated the Bed model with new information

--- src/models/bed.model.ts
+++ src/models/bed.model.ts
@@ -3,11 +3,22 @@ export class BedModel {
   public title: string;
   public bed_location: string;
   public beds_in_room: number;
-  public booked=0; //number of affl or zero
+  public location: string;
+  public rating: number;
+  public wifi: boolean;
+  public basics: string;
+  public practices: string;
+  public booked: boolean; //number of affl or zero
   public hash?: string;
   constructor(){
     this.title = "Bed 0";
     this.bed_location = "TC-23.0";
     this.beds_in_room = 220;
+    this.location = "Brummen";
+    this.rating = 5;
+    this.wifi = true;
+    this.basics = "22P5 + delurant";
+    this.practices = "medicatie";
+    this.booked = true;
   }
 }
